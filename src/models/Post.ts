72ed6ba00be870a8d76ebb2cb3c8aa3a6348148d
wilsonbregeon <wilsonbregeon@gmail.com>
2023-03-12 15:58:17 -0300

    Modelagem das tipagens das entidades

---
+++ src/models/Post.ts
@@ -0,0 +1,105 @@
+import { PostDB, PostModel } from "../types";
+
+export class Post {
+    constructor(
+        private id: string,
+        private name: string,
+        private likes: number,
+        private dislikes: number,
+        private createdAt: string,
+        private updatedAt: string,
+        private creatorId: string,
+        private creatorName: string
+    ) { }
+
+    public getId(): string {
+        return this.id
+    } 
+
+    public setId(value: string): void {
+        this.id = value
+    }
+
+    public getName(): string {
+        return this.name
+    } 
+
+    public setName(value: string): void {
+        this.name = value
+    }
+
+    public getLikes(): number {
+        return this.likes
+    } 
+
+    public setLikes(value: number): void {
+        this.likes = value
+    }
+
+    public getDislikes(): number {
+        return this.dislikes
+    } 
+
+    public setDislikes(value: number): void {
+        this.dislikes = value
+    }
+
+    public getCreatedAt(): string {
+        return this.createdAt
+    } 
+
+    public setCreatedAt(value: string): void {
+        this.createdAt = value
+    }
+
+    public getUpdatedAt(): string {
+        return this.updatedAt
+    } 
+
+    public setUpdatedAt(value: string): void {
+        this.updatedAt = value
+    }
+
+    public getCreatorId(): string {
+        return this.createdAt
+    } 
+
+    public setCreatorId(value: string): void {
+        this.creatorId = value
+    }
+
+    public getCreatorName(): string {
+        return this.creatorName
+    } 
+
+    public setCreatorName(value: string): void {
+        this.creatorName = value
+    }
+
+    public toDBModel(): PostDB {
+        return {
+            id: this.id,
+            creator_id: this.creatorId,
+            name: this.name,
+            likes: this.likes,
+            dislikes: this.dislikes,
+            created_at: this.createdAt,
+            updated_at: this.updatedAt
+        }
+    }
+
+    public toBusinessModel(): PostModel {
+        return {
+            id: this.id,
+            name: this.name,
+            likes: this.likes,
+            dislikes: this.dislikes,
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt,
+            creator: {
+                id: this.creatorId,
+                name: this.creatorName
+            }
+        }
+    }
+}

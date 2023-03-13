a6a09e936ec394da07150e1a8af8f0dedaeaa008
wilsonbregeon <wilsonbregeon@gmail.com>
2023-03-12 22:54:38 -0300

    Implementação signup

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,7 +1,30 @@
-import {UserBusiness} from "../business/UserBusiness"
+import { Request, Response } from "express"
+import { UserBusiness } from "../business/UserBusiness"
+import { SignupInputDTO } from "../dtos/userDTO"
+import { BaseError } from "../errors/BaseError"
 
 export class UserController {
     constructor(
         private userBusiness: UserBusiness
-    ) {}
+    ) { }
+
+    public signup = async (req: Request, res: Response) => {
+        try {
+            const input: SignupInputDTO = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+
+            const output = await this.userBusiness.signup(input)
+
+            res.status(201).send(output)
+        } catch (error) {
+            if (error instanceof BaseError) {
+                res.status(error.statusCode).send(error.message)
+            } else {
+                res.status(500).send("Erro inesperado!")
+            }
+        }
+    }
 }
--- src/database/UserDatabase.ts
+++ src/database/UserDatabase.ts
@@ -1,6 +1,12 @@
 import { BaseDatabase } from "./BaseDatabase";
-
+import { UserDB } from "../types"
 
 export class UserDatabase extends BaseDatabase {
     public static TABLE_USERS = "users"
+
+    public insert = async (userDB: UserDB) => {
+        await BaseDatabase
+        .connection(UserDatabase.TABLE_USERS)
+        .insert(userDB)
+    }
 }

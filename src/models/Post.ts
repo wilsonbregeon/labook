fcf7e03319a4bce240c2e66adb4bc9284ceb454e
wilsonbregeon <wilsonbregeon@gmail.com>
2023-03-15 23:50:16 -0300

    Implementação de like ou dislike no post

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -36,6 +36,22 @@ export class Post {
         this.likes = value
     }
 
+    public addLike() {
+        this.likes += 1
+    }
+
+    public removeLike() {
+        this.likes -= 1
+    }
+
+    public addDislike() {
+        this.dislikes += 1
+    }
+
+    public removeDislike() {
+        this.dislikes -= 1
+    }
+
     public getDislikes(): number {
         return this.dislikes
     } 
--- src/types.ts
+++ src/types.ts
@@ -36,6 +36,17 @@ export interface PostWithCreatorDB extends PostDB {
     creator_name: string
 }
 
+export interface LikeDislikeDB {
+    user_id: string,
+    post_id: string,
+    like: number
+}
+
+export enum POST_LIKE {
+    ALREADY_LIKED = "ALREADY_LIKED",
+    ALREADY_DISLIKED = "ALREADY_DISLIKED"
+}
+
 export interface UserDB {
     id: string,
     name: string,
